9ac1036077cbbc25bf993c6c434ec75bf53b8567
jorgeluis1985 <jorgeluiscamarcobaldovino@gmail.com>
2023-09-05 09:58:48 +0200

    ajout du component error404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
+import { Error404Component } from './error404/error404.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'article', component: ArticleComponent}
+  {path: 'article', component: ArticleComponent}, 
+  {path: '**',component: Error404Component}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CardComponent } from './card/card.component';
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     CardComponent,
     HomeComponent,
     ArticleComponent,
-    NavbarComponent
+    NavbarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
